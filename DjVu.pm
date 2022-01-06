c9cc7b327db0544074b63233fd01bf06d4661885
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-01-06 15:23:47 +0100

    Prepare for 0.03 version

--- DjVu.pm
+++ DjVu.pm
@@ -9,7 +9,7 @@ use File::Basename;
 use Getopt::Std;
 use Perl6::Slurp qw(slurp);
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Constructor.
 sub new {
@@ -183,6 +183,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
